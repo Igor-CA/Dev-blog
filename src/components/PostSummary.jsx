ab660b30fc496c28cce9f9d5949325765b1919d8
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2024-03-27 17:51:04 -0300

    Post summary redesign

--- src/components/PostSummary.jsx
+++ src/components/PostSummary.jsx
@@ -1,13 +1,26 @@
-import Markdown from "react-markdown"
-import "../index.css"
+import { Link } from "react-router-dom";
+import "../index.css";
 
-export default function PostSummary({post}){
-    const {title, author, content} = post
-    return(
-        <div className="post-summary">
-            <strong>{title}</strong><br />
-            <small>{author}</small>
-            <Markdown>{content}</Markdown>
-        </div>
-    )
+export default function PostSummary({ post }) {
+	const { title, tags, description, cover, date, id } = post;
+	const formatedDate = new Date(date).toLocaleDateString();
+	return (
+		<Link className="post-summary" to={`./post/${id}`}>
+			<img
+				src={`http://${window.location.hostname}:3000/assets/${cover}`}
+				alt="cover"
+				className="post-summary__cover"
+			/>
+			<ul className="post-summary__tags">
+				{tags.map((tag, i) => {
+					return <li key={i}>{tag}</li>;
+				})}
+			</ul>
+			<section className="post-summary__content">
+				<h2>{title}</h2>
+				<p>{formatedDate}</p>
+				<p>{description}</p>
+			</section>
+		</Link>
+	);
 }
