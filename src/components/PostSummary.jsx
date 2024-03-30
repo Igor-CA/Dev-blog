cf56ceb04631fb720351680012c4ed4748746f1e
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2024-03-30 16:51:05 -0300

    Redesign usin taiwind

--- src/components/PostSummary.jsx
+++ src/components/PostSummary.jsx
@@ -1,24 +1,33 @@
 import { Link } from "react-router-dom";
-import "../index.css";
 
 export default function PostSummary({ post }) {
 	const { title, tags, description, cover, date, id } = post;
-	const formatedDate = new Date(date).toLocaleDateString();
+	const formatedDate = new Date(date).toLocaleDateString(undefined,{year: 'numeric', month: 'long', day: 'numeric' });
+	console.log({ date, formatedDate });
 	return (
-		<Link className="post-summary" to={`./post/${id}`}>
-			<img
-				src={`http://${window.location.hostname}:3000/assets/${cover}`}
-				alt="cover"
-				className="post-summary__cover"
-			/>
-			<ul className="post-summary__tags">
-				{tags.map((tag, i) => {
-					return <li key={i}>{tag}</li>;
-				})}
-			</ul>
-			<section className="post-summary__content">
-				<h2>{title}</h2>
-				<p>{formatedDate}</p>
+		<Link className="inline-block border border-slate-700 rounded-md overflow-hidden w-full" to={`./post/${id}`}>
+			<div className="relative">
+				<img
+					src={`http://${window.location.hostname}:3000/assets/${cover}`}
+					alt="cover"
+					className="w-full border border-slate-700"
+				/>
+				<ul className="absolute bottom-0 right-0 flex gap-2.5 px-2.5">
+					{tags.map((tag, i) => {
+						return (
+							<li
+								key={i}
+								className="bg-slate-700 p-2.5 rounded-md my-2.5 text-slate-50 border border-slate-50"
+							>
+								{tag}
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+			<section className="p-2.5">
+				<h2 className="font-bold text-lg">{title}</h2>
+				<p className="text-sm">{formatedDate}</p>
 				<p>{description}</p>
 			</section>
 		</Link>
--- src/pages/AllPostsPage.jsx
+++ src/pages/AllPostsPage.jsx
@@ -1,10 +1,14 @@
 import postsList from "../posts.json";
 import PostSummary from "../components/PostSummary";
 
-export default function AllPostsPage({language}) {
-	return postsList.map((post) => {
-		return post.language === language ? (
-			<PostSummary key={post.id} post={post}></PostSummary>
-		) : null;
-	});
+export default function AllPostsPage({ language }) {
+	return (
+		<div className="p-5 md:grid md:grid-cols-2 md:gap-4 lg:grid-cols-3">
+			{postsList.map((post) => {
+				return post.language === language ? (
+					<PostSummary key={post.id} post={post}></PostSummary>
+				) : null;
+			})}
+		</div>
+	);
 }
