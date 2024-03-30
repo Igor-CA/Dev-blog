7a6637f1f1b0665971cdfabb0c5ebbdceac66acd
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2024-03-30 16:51:49 -0300

    Added a header as a navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import "./index.css";
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LanguageToggle from "./components/LanguageToggle";
 import AllPostsPage from "./pages/AllPostsPage";
 import PostPage from "./pages/PostPage";
+import MainHeader from "./components/MainHeader";
 
 function App() {
 	return (
-		<div className="App">
+		<div className="bg-slate-50 min-h-svh">
 			<BrowserRouter>
-				<LanguageToggle></LanguageToggle>
+				<MainHeader></MainHeader>
 				<Routes>
-					<Route path="/pt-br" element={<AllPostsPage language={"pt-br"} />}></Route>
-					<Route path="/" element={<AllPostsPage language={"en"}/>}></Route>
-					<Route path="/pt-br/post/:id" element={<PostPage language={"pt-br"} />}></Route>
-					<Route path="/post/:id" element={<PostPage language={"en"} />}></Route>
+					<Route
+						path="/pt-br"
+						element={<AllPostsPage language={"pt-br"} />}
+					></Route>
+					<Route path="/" element={<AllPostsPage language={"en"} />}></Route>
+					<Route
+						path="/pt-br/post/:id"
+						element={<PostPage language={"pt-br"} />}
+					></Route>
+					<Route
+						path="/post/:id"
+						element={<PostPage language={"en"} />}
+					></Route>
 					<Route path="*" element={<p>Página não encontrada</p>}></Route>
 				</Routes>
 			</BrowserRouter>
---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import LanguageToggle from "./LanguageToggle";
+import { LuSunDim } from "react-icons/lu";
+
+
+export default function MainHeader() {
+	const [currentLanguage, setCurrentLanguage] = useState(
+		window.location.pathname.startsWith("/pt-br") ? "pt-br" : "en"
+	);
+	const location = useLocation();
+	useEffect(() => {
+		setCurrentLanguage(
+			window.location.pathname.startsWith("/pt-br") ? "pt-br" : "en"
+		);
+	}, [location]);
+
+	return (
+		<header className="w-full bg-slate-50/80 backdrop-blur-md  sticky top-0 z-10">
+			<div className="w-full bg-slate-700 h-1.5"></div>
+			<div className="flex justify-around items-center">
+				<Link to={currentLanguage === "pt-br" ? "/pt-br" : "/"} className="font-bold text-xl p-2.5">Blog</Link>
+				<button className="border border-slate-700 text-2xl p-1 rounded-md"><LuSunDim /></button>
+				<LanguageToggle></LanguageToggle>
+			</div>
+		</header>
+	);
+}
