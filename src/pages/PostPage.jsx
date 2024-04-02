7f263e4b0caa62277893fec9e2a2337fe0cc4793
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2024-04-02 14:28:00 -0300

    Add responsiveness to page

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -30,37 +30,60 @@ export default function PostPage({ language }) {
 			day: "numeric",
 		});
 		return (
-			<div className="px-6 py-2.5">
-				<img
-					src={`http://${window.location.hostname}:3000/assets/${cover}`}
-					alt="cover"
-					className="w-full border border-slate-700 rounded-md"
-				/>
-				<h1 className="font-bold text-2xl">{title}</h1>
-				{language === "pt-br" && (
-					<p>
-						Escrito por {author} on {formatedDate}{" "}
-					</p>
-				)}
-				{language === "en" && (
-					<p>
-						Written by {author} on {formatedDate}{" "}
-					</p>
-				)}
-				{tags.map((tag) => {
-					return <span className="mx-2">#{tag} </span>;
-				})}
-				<hr className="border border-slate-600/60 mb-4" />
-				<article className="markdown">
-					<Markdown
-						remarkPlugins={[remarkGfm]}
-						components={{
-							code: CodeBlock,
-						}}
-					>
-						{content}
-					</Markdown>
-				</article>
+			<div className="px-4 py-2.5 max-w-5xl mx-auto">
+				<div className="mx-auto">
+					<img
+						src={`http://${window.location.hostname}:3000/assets/${cover}`}
+						alt="cover"
+						className="w-full border border-slate-700 rounded-md mb-5"
+					/>
+					<h1 className="font-extrabold text-slate-900 text-4xl mb-2">
+						{title}
+					</h1>
+					{language === "pt-br" && (
+						<p>
+							Escrito por {author} on {formatedDate}{" "}
+						</p>
+					)}
+					{language === "en" && (
+						<p>
+							Written by {author} on {formatedDate}{" "}
+						</p>
+					)}
+					<div className="my-2">
+						{tags.map((tag) => {
+							return <span className="mx-2">#{tag} </span>;
+						})}
+					</div>
+					<hr className="border border-slate-700/60 mb-4" />
+				</div>
+				<section className="mt-4 lg:grid lg:grid-cols-[auto,1fr] ">
+					<article className="prose prose-slate max-w-4xl w-full break-words lg:max-w-2xl">
+						<Markdown
+							remarkPlugins={[remarkGfm]}
+							components={{
+								code: CodeBlock,
+							}}
+						>
+							{content}
+						</Markdown>
+					</article>
+					<aside class="sticky top-16 grid gap-4 self-start lg:w-64  justify-self-end">
+						<section class="hidden gap-4 lg:grid">
+							<h2 class="text-xl font-bold text-slate-900 dark:text-slate-100">
+								Table of Contents
+							</h2>
+							<nav class="grid justify-items-start gap-2 text-sm font-medium">
+								<a
+									class="rounded-md transition-shadow focus-visible:outline-none focus-visible:ring-2 text-slate-900 dark:text-slate-100"
+									href="#exemple1"
+								>
+									Exemple of content
+								</a>
+							</nav>
+						</section>
+					</aside>
+				</section>
 			</div>
 		);
 	}
