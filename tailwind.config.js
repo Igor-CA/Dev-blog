b571555471469abd1d45442a6d6d7668c2fc0930
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2024-04-02 14:22:09 -0300

    Use of typography plugin tailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+const disabledCss = {
+	"blockquote p:first-of-type::before": false,
+	"blockquote p:last-of-type::after": false,
+	pre: false,
+	code: false,
+	"pre code": false,
+};
+
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			typography: {
+				DEFAULT: { css: disabledCss },
+				sm: { css: disabledCss },
+				lg: { css: disabledCss },
+				xl: { css: disabledCss },
+				"2xl": { css: disabledCss },
+			},
+		},
 	},
-	plugins: [],
+	plugins: [require("@tailwindcss/typography")],
 };
