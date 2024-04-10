af27c8d0526557c30fb41bcac6a095f2d185ae1e
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2024-04-10 13:57:19 -0300

    Remove of unnecessary parts

--- src/hooks/useActiveHeading.js
+++ src/hooks/useActiveHeading.js
@@ -41,26 +41,3 @@ export default function useActiveHeader() {
 	}, [setActiveId]);
 	return activeId;
 }
-
-export function useScrollSpy(ids, options) {
-	const [activeId, setActiveId] = useState();
-	const observer = useRef();
-	useEffect(() => {
-		const elements = ids.map((id) => document.getElementById(id));
-		observer.current?.disconnect();
-		observer.current = new IntersectionObserver((entries) => {
-			entries.forEach((entry) => {
-				if (entry?.isIntersecting) {
-					setActiveId(entry.target.id);
-				}
-			});
-		}, options);
-		elements.forEach((el) => {
-			if (el) {
-				observer.current?.observe(el);
-			}
-		});
-		return () => observer.current?.disconnect();
-	}, [ids, options]);
-	return activeId;
-}
